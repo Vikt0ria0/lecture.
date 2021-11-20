631d3e1820e33dc760dd2d9fb75388b979d71e16
Vikt0ria0 <91361993+Vikt0ria0@users.noreply.github.com>
2021-11-20 21:42:13 +0700

    Lecture04

---
+++ Lecture04 3.py
@@ -0,0 +1,31 @@
+import random
+
+N = input('Введите количество рандомных чисел: ')
+N = int(N)
+
+def bogoSort(a):
+    n = len(a)
+    while (is_sorted(a) == False):
+        shuffle(a)
+
+def is_sorted(a):
+    n = len(a)
+    for i in range(0, n - 1):
+        if (a[i] > a[i + 1]):
+            return False
+        return True
+
+def shuffle(a):
+    n = len(a)
+    for i in range(0, n):
+        r = random.randint(0,n - 1)
+        a[i], a[r] = a[r], a[i]
+
+a = []
+for i in range(N):
+    a.append(random.randint(1, 1000))
+
+bogoSort(a)
+print('sort: ')
+for i in range(len(a)):
+    print(a[i])
